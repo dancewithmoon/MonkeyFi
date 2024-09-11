c652a982a6b58976a9be4e9a73a209b6597dfc3a
Ihor Reshetnykov <ihor.reshetnykov.workonly@gmail.com>
2024-09-11 21:21:37 +0300

    MONK-2: Hide the current window on calling ShowWindow

--- Assets/Project/Main/Scripts/Services/WindowService/WindowService.cs
+++ Assets/Project/Main/Scripts/Services/WindowService/WindowService.cs
@@ -9,6 +9,8 @@ namespace Services
         private readonly IGameFactory _gameFactory;
         private readonly Dictionary<WindowType, BaseWindow> _windows = new();
 
+        private WindowType _currentWindow = WindowType.None;
+        
         public WindowService(IGameFactory gameFactory)
         {
             _gameFactory = gameFactory;
@@ -16,14 +18,26 @@ namespace Services
 
         public void ShowWindow(WindowType windowType)
         {
+            HideWindow(_currentWindow);
+
+            if(windowType == WindowType.None)
+                return;
+            
             if (_windows.ContainsKey(windowType))
                 _windows[windowType].Visible = true;
             else
                 _windows[windowType] = _gameFactory.CreateWindow(windowType);
+            
+            _currentWindow = windowType;
         }
 
         public void ShowWindow<TPayload>(WindowType windowType, TPayload payload)
         {
+            HideWindow(_currentWindow);
+            
+            if(windowType == WindowType.None)
+                return;
+            
             ShowWindow(windowType);
             if (_windows[windowType] is PayloadedWindow<TPayload> payloadedWindow)
                 payloadedWindow.SetPayload(payload);
@@ -31,6 +45,9 @@ namespace Services
 
         public void HideWindow(WindowType windowType)
         {
+            if(windowType == WindowType.None)
+                return;
+            
             _windows[windowType].Visible = false;
         }
     }
