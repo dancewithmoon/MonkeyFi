c73e104f645e57277aca5dcc9049be4c03165b2b
Ihor Reshetnykov <ihor.reshetnykov.workonly@gmail.com>
2024-09-12 23:04:36 +0300

    MONK-21: Add UserData service

---
+++ Assets/Project/Main/Scripts/Services/UserDataService.cs
@@ -0,0 +1,15 @@
+﻿using Services.Telegram;
+
+namespace Services
+{
+    public class UserDataService
+    {
+        private readonly TelegramService _telegramService;
+        public string Username => _telegramService.TelegramUser.Username;
+        
+        public UserDataService(TelegramService telegramService)
+        {
+            _telegramService = telegramService;
+        }
+    }
+}
