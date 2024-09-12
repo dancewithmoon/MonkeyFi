da90a2cd6995fa8ef0fe7894b3501ca963237d82
Ihor Reshetnykov <ihor.reshetnykov.workonly@gmail.com>
2024-09-12 23:21:20 +0300

    MONK-21: Add telegram username to Friends window

--- Assets/Project/Main/Scripts/UI/Windows/FriendsWindow.cs
+++ Assets/Project/Main/Scripts/UI/Windows/FriendsWindow.cs
@@ -1,7 +1,25 @@
-﻿namespace UI.Windows
+﻿using Services;
+using TMPro;
+using UnityEngine;
+using Zenject;
+
+namespace UI.Windows
 {
     public class FriendsWindow : BaseWindow
     {
+        [SerializeField] private TMP_Text _userDataText;
         
+        private UserDataService _userDataService;
+
+        [Inject]
+        private void Construct(UserDataService userDataService)
+        {
+            _userDataService = userDataService;
+        }
+
+        public override void DrawWindow()
+        {
+            _userDataText.text = _userDataService.Username;
+        }
     }
 }
