62e83ca72cc3c3470085422ca6cb85a5471f6d5d
Ihor Reshetnykov <ihor.reshetnykov.workonly@gmail.com>
2024-09-12 18:39:18 +0300

    MONK-21: Add Telegram plugin

---
+++ Assets/Plugins/Telegram/TelegramBridge.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+namespace Plugins.Telegram
+{
+    public class TelegramBridge : MonoBehaviour
+    {
+        public event Action<TelegramUserDto> OnUserDataReceiveEvent;
+        
+        public void ReceiveUserData(string data)
+        {
+            var user = JsonUtility.FromJson<TelegramUserDto>(data);
+            OnUserDataReceiveEvent?.Invoke(user);
+        }
+        
+        [DllImport("__Internal")]
+        public static extern void RequestUserData();
+
+        [DllImport("__Internal")]
+        public static extern void ShowMainButton(string text);
+
+        [DllImport("__Internal")]
+        public static extern void HideMainButton();
+
+        [DllImport("__Internal")]
+        public static extern void MainButtonShowProgress();
+
+        [DllImport("__Internal")]
+        public static extern void MainButtonHideProgress();
+
+        [DllImport("__Internal")]
+        public static extern void ShowBackButton();
+
+        [DllImport("__Internal")]
+        public static extern void HideBackButton();
+
+        [DllImport("__Internal")]
+        public static extern void ShowAlert(string text);
+
+        [DllImport("__Internal")]
+        public static extern void ShowShareJoinCode(string code);
+
+        [DllImport("__Internal")]
+        public static extern void Ready();
+
+        [DllImport("__Internal")]
+        public static extern void Close();
+
+        [DllImport("__Internal")]
+        public static extern void Expand();
+
+        [DllImport("__Internal")]
+        public static extern void HapticFeedback(string level);
+
+        [DllImport("__Internal")]
+        public static extern void ShowScanQrPopup(string text);
+
+        [DllImport("__Internal")]
+        public static extern void CloseScanQrPopup();
+    }
+}
