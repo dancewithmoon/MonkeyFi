a392b2fe851901107a76f993a032e20f9030d795
Ihor Reshetnykov <ihor.reshetnykov.workonly@gmail.com>
2024-09-12 23:04:08 +0300

    MONK-21: Add Telegram service

--- Assets/Plugins/Telegram/TelegramUserDto.cs
+++ Assets/Plugins/Telegram/TelegramUserDto.cs
@@ -4,14 +4,10 @@
     public class TelegramUserDto
     {
         public int id;
-        public bool is_bot;
         public string first_name;
         public string last_name;
         public string username;
         public string language_code;
-        public bool is_premium;
-        public bool added_to_attachment_menu;
         public bool allows_write_to_pm;
-        public string photo_url;
     }
 }
---
+++ Assets/Project/Main/Scripts/Services/Telegram/TelegramService.cs
@@ -0,0 +1,29 @@
+﻿using Plugins.Telegram;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace Services.Telegram
+{
+    public class TelegramService
+    {
+        public TelegramUserData TelegramUser { get; private set; }
+        
+        public void Initialize()
+        {
+            InitializeBridge();
+            TelegramBridge.RequestUserData();
+        }
+
+        private void InitializeBridge()
+        {
+            var bridge = new GameObject("TelegramBridge").AddComponent<TelegramBridge>();
+            Object.DontDestroyOnLoad(bridge);
+            bridge.OnUserDataReceiveEvent += OnUserDataReceive;
+        }
+        
+        private void OnUserDataReceive(TelegramUserDto user)
+        {
+            TelegramUser = new TelegramUserData(user.username);
+        }
+    }
+}
