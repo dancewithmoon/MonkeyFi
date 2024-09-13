03fc9bce804bad4bad6312728592a36dcea1735e
Ihor Reshetnykov <ihor.reshetnykov.workonly@gmail.com>
2024-09-13 23:10:49 +0300

    MONK-3: Add Config static data

--- Assets/Project/Main/Scripts/Infrastructure/StaticData/Services/IStaticDataService.cs
+++ Assets/Project/Main/Scripts/Infrastructure/StaticData/Services/IStaticDataService.cs
@@ -1,11 +1,13 @@
 ﻿using Base.States;
 using Services;
+using StaticData;
 using UI.Windows;
 
 namespace Infrastructure.StaticData.Services
 {
     public interface IStaticDataService : IPreloadedInBootstrap
     {
+        public ConfigStaticData GetConfig();
         public BaseWindow GetWindowPrefab(WindowType windowType);
     }
 }
---
+++ Assets/Project/Main/Scripts/StaticData/ConfigStaticData.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace StaticData
+{
+    [CreateAssetMenu(fileName = "Config", menuName = "ScriptableObjects/Config", order = 0)]
+    public class ConfigStaticData : ScriptableObject
+    {
+        [SerializeField] private int _defaultMaxEnergy;
+
+        public int DefaultMaxEnergy => _defaultMaxEnergy;
+    }
+}
